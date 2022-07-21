c4345b34d17b906ebd0cc2ce35417eac49a731be
Pavel Fokin <pavelfokin@A125372.local>
2022-07-21 16:34:30 +0200

    Add style to the 'Like It!' button

--- frontend/static/app.js
+++ frontend/static/app.js
@@ -1,11 +1,25 @@
 function App() {
-    return (
-        <button
-            onClick={() => { console.log("Clicked") }}
-        >
-            Like It
-        </button>
-    )
+  return (
+    <div className="mx-auto flex flex-1 flex-col items-center justify-center">
+      <button
+        className="relative inline-block text-lg group"
+        onClick={() => {
+          console.log("Clicked");
+        }}
+      >
+        <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+          <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+          <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+          <span className="relative">Like It!</span>
+        </span>
+        <span
+          className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+          data-rounded="rounded-lg"
+        ></span>
+      </button>
+      <h1 className="mt-2">0 people liked this page</h1>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("app"));
