09262ba41386a1e9187ec705d1ca130ab1af4bda
Pavel Fokin <fokinpv@gmail.com>
2022-07-23 08:38:15 +0200

    Serve static content from the 'dist' directory

--- main.go
+++ main.go
@@ -2,28 +2,22 @@ package main
 
 import (
 	"embed"
-	"io"
 	"io/fs"
 	"log"
 	"net/http"
 )
 
 var (
-	//go:embed frontend
-	content embed.FS
-
-	//go:embed frontend/index.html
-	index string
+	//go:embed web
+	dist embed.FS
 )
 
 func main() {
-	static, _ := fs.Sub(fs.FS(content), "frontend")
+	web, _ := fs.Sub(fs.FS(dist), "web")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
-		io.WriteString(w, index)
-	})
 	http.Handle(
-		"/static/", http.FileServer(http.FS(static)))
+		"/", http.FileServer(http.FS(web)),
+	)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
