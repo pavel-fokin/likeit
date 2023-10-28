c4cebc75e1752a990df21cfd644b3dc80a48ccfb
Pavel Fokin <fokinpv@gmail.com>
2023-10-28 19:42:41 +0200

    [fe] Move frontend code to 'src'

--- web/index.js
+++ web/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 
-import { App } from './App';
+import { App } from './src/App';
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(<App />);
